970d0153afc2688066f8933e352029c7170a9bd9
chenyong8888 <359492089@qq.com>
2012-09-12 16:37:16 +0800

    Update application/views/qqweibo/callback.php

--- application/views/qqweibo/callback.php
+++ application/views/qqweibo/callback.php
@@ -1,1 +1,1 @@
-授权完成,<a href="http://10086.client.sina.com.cn/index.php?control=QQWeibo&action=userInfo">进入你的微博列表页面</a><br />
+授权完成,<a href="xxxxxxxxxxxxxxxxxxxxxxxx?control=QQWeibo&action=userInfo">进入你的微博列表页面</a><br />

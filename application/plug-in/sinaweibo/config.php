67c841d0af004c1370c9f2acc6c354401953397e
chenyong8888 <359492089@qq.com>
2012-09-12 16:35:26 +0800

    Update application/plug-in/sinaweibo/config.php

--- application/plug-in/sinaweibo/config.php
+++ application/plug-in/sinaweibo/config.php
@@ -3,4 +3,4 @@ header('Content-Type: text/html; charset=UTF-8');
 
 define( "sina_WB_AKEY" , 'xxxxxxxx' );
 define( "sina_WB_SKEY" , 'xxxxxxxxxxxxxxxxxxxxxxx' );
-define( "sina_WB_CALLBACK_URL" , 'http://malibumytime.vic.sina.com.cn/index.php?control=SinaWeibo&action=callBack' );
+define( "sina_WB_CALLBACK_URL" , 'xxxxxxxxxxxxxxxxxxxxxxxxx?control=SinaWeibo&action=callBack' );

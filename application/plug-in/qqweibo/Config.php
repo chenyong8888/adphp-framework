3fcf666e8ab3482f1216f821cd271c1d1c5f32de
chenyong8888 <359492089@qq.com>
2012-09-12 16:34:44 +0800

    Update application/plug-in/qqweibo/Config.php

--- application/plug-in/qqweibo/Config.php
+++ application/plug-in/qqweibo/Config.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type: text/html; charset=UTF-8');
 
-define( "qq_client_id" , 'd504caa4de24446fb9a2f40cfd8d9c9c' );
-define( "qq_client_secret" , '05c2e5e324cb1a5ff1479a01b3b3e55f' );
+define( "qq_client_id" , 'xxxxxxxxxxxxxxxxxxxxx' );
+define( "qq_client_secret" , 'xxxxxxxxxxxxxxxxxxxxxxxxxxx' );
 define( "qq_debug" , false );
-define( "qq_callback_url" , 'http://10086.client.sina.com.cn/index.php?control=QQWeibo&action=callBack' );
+define( "qq_callback_url" , 'xxxxxxxxxxxxxxxxxxxxxxxxxx?control=QQWeibo&action=callBack' );

a7db25ebe8f0ba642895273c1721755e20e8f176
chenyong8888 <359492089@qq.com>
2012-09-12 16:38:22 +0800

    Update application/views/sinaweibo/callback.php

--- application/views/sinaweibo/callback.php
+++ application/views/sinaweibo/callback.php
@@ -1,1 +1,1 @@
-授权完成,<a href="http://malibumytime.vic.sina.com.cn/index.php?control=SinaWeibo&action=userInfo">进入你的微博列表页面</a><br />
+授权完成,<a href="?control=SinaWeibo&action=userInfo">进入你的微博列表页面</a><br />
